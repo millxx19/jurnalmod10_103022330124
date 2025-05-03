34f0ccecfc9089f123f9449dca1c8e9a3d27ac10
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-05-03 17:49:30 +0700

    menambahkan API Movies

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jurnalmod10_103022330124.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+        private static readonly List<movie> movies = new List<movie>
+        {
+            new movie("The Shawshank Redemption", "Frank Darabont", new List<string> { "Tim Robbins", "Morgan Freeman" , "Bob Gunton" }, "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new movie("The Godfather", "Francis Ford Coppola", new List<string> { "Marlon Brando", "AI Pacino" }, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
+            new movie("The Dark Knight", "Christopher Nolan", new List<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart" }, "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
+        };
+        [HttpGet]
+        public IEnumerable<movie> Get()
+        {
+            return movies;
+        }
+        [HttpGet("{id}")]
+        public movie Get(int id)
+        {
+            return movies[id];
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            movies.RemoveAt(id);
+        }
+        [HttpPost]
+        public void Post([FromBody] movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
---
+++ Controllers/movie.cs
@@ -0,0 +1,18 @@
+﻿namespace jurnalmod10_103022330124.Controllers
+{
+    public class movie
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+        public movie(string title, string director, List<string> Stars, string Description)
+        {
+            this.title = title;
+            this.director = director;
+            this.Stars = Stars;
+            this.Description = Description;
+        }
+    }
+}
+
